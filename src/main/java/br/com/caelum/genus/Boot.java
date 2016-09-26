87e9c2c3e7b8bc60bd4b38ce55b97367220995b6
Joviane <joviane@gmail.com>
2016-09-26 18:31:26 -0300

    Tentando parar com os erros de 404 no Heroku

--- src/main/java/br/com/caelum/genus/Boot.java
+++ src/main/java/br/com/caelum/genus/Boot.java
@@ -5,36 +5,22 @@ import java.security.Principal;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-@EnableOAuth2Sso
 @RestController
 @EntityScan(basePackageClasses = { Boot.class, Jsr310JpaConverters.class })
-public class Boot extends WebSecurityConfigurerAdapter {
+public class Boot extends SpringBootServletInitializer {
 
 	@RequestMapping("/user")
 	public Principal user(Principal principal) {
 		return principal;
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers(HttpMethod.PATCH, "/trainee/changeStatus").permitAll()
-				.antMatchers("/", "/login/**").permitAll()
-				.anyRequest().authenticated()
-				.and().formLogin().loginPage("/")
-				.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/exit")
-				.and().csrf().disable();
 	}
 	
 	@Bean
@@ -48,5 +34,10 @@ public class Boot extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Boot.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Boot.class);
 	}
 }
